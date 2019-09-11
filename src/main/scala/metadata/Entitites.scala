010755f00166e2a99d4da28454edc3b82ed73e6e
Dragisa Krsmanovic <dragishak@gmail.com>
2019-09-11 15:53:19 -0700

    More

--- src/main/scala/metadata/Entitites.scala
+++ src/main/scala/metadata/Entitites.scala
@@ -4,6 +4,8 @@ import com.dragisak.typelevel.HList.HNil
 import com.dragisak.typelevel.{<<:, HList}
 import com.dragisak.typelevel.HList._
 
+import scala.collection.IndexedSeqView.Id
+
 sealed trait FieldType
 
 object FieldType {
@@ -23,44 +25,70 @@ object Mandatory {
   sealed trait No  extends Mandatory
 }
 
-trait FieldDef[Type <: FieldType, M <: Mandatory]
+trait FieldDef[T <: FieldType, M <: Mandatory] {
 
-trait EntityDef[Field <: HList, FieldGroup <: FieldDef[_, _]]
+  type Def = this.type
+
+  def name: String
+}
+
+object FieldDef {
+  def apply[T <: FieldType, M <: Mandatory](n: String): FieldDef[T, M] = new FieldDef[T, M] {
+    override val name: String = n
+  }
+}
+
+trait EntityDef[Field <: HList, FieldGroup <: FieldDef[_, _]] {
+
+  type Def = this.type
+
+  def name: String
+}
 
 object EntityDef {
-  def apply[Fields <: HList, FieldGroup <: FieldDef[_, _]](
+  def apply[Fields <: HList, FieldGroup <: FieldDef[_, _]](n: String)(
       implicit ev1: Fields <<: FieldGroup
-  ): EntityDef[Fields, FieldGroup] = null
+  ): EntityDef[Fields, FieldGroup] = new EntityDef[Fields, FieldGroup] { override val name: String = n }
 }
 
 object Metadata {
 
   trait Entity {
     sealed trait FieldGroup[T <: FieldType, M <: Mandatory] extends FieldDef[T, M]
+
+    protected def field[T <: FieldType, M <: Mandatory](n: String): FieldGroup[T, M] = new FieldGroup[T, M] {
+      override val name: String = n
+    }
+
     type Fields <: HList
     final type Def = EntityDef[Fields, FieldGroup[_, _]]
+
+    protected def entity(n: String)(implicit ev: Fields <<: FieldGroup[_, _]): Def = EntityDef(n)
+
     def value: Def
   }
 
 //  def entityValue[T <: Entity](implicit ev: T#Fields <<: T#FieldGroup): T#Def = EntityDef[T#Fields, T#FieldGroup](ev)
 
   object Organization extends Entity {
-    type Id              = FieldGroup[FieldType.Id, Mandatory.Yes]
-    type Name            = FieldGroup[FieldType.Text, Mandatory.Yes]
-    type Founders        = FieldGroup[FieldType.Coll[FieldType.Id], Mandatory.No]
-    override type Fields = Id :: Name :: Founders :: HNil
+    val Id       = field[FieldType.Id, Mandatory.Yes]("id")
+    val Name     = field[FieldType.Text, Mandatory.Yes]("name")
+    val Founders = field[FieldType.Coll[FieldType.Id], Mandatory.No]("founders")
 
-    override val value: Def = EntityDef[Fields, FieldGroup[_, _]]
+    override final type Fields = Id.Def :: Name.Def :: Founders.Def :: HNil
+
+    override val value: Def = entity("organization")
   }
 
   object FundingRound extends Entity {
-    type Id              = FieldGroup[FieldType.Id, Mandatory.Yes]
-    type Round           = FieldGroup[FieldType.Text, Mandatory.Yes]
-    type Amount          = FieldGroup[FieldType.Money, Mandatory.Yes]
-    type Investors       = FieldGroup[FieldType.Coll[FieldType.Id], Mandatory.No]
-    type Investee        = FieldGroup[FieldType.Id, Mandatory.No]
-    override type Fields = Id :: Round :: Amount :: Investee :: Investors :: HNil
+    val Id        = field[FieldType.Id, Mandatory.Yes]("id")
+    val Round     = field[FieldType.Text, Mandatory.Yes]("round")
+    val Amount    = field[FieldType.Money, Mandatory.Yes]("amount")
+    val Investors = field[FieldType.Coll[FieldType.Id], Mandatory.No]("investors")
+    val Investee  = field[FieldType.Id, Mandatory.No]("investee")
 
-    override val value: Def = EntityDef[Fields, FieldGroup[_, _]]
+    override final type Fields = Id.Def :: Round.Def :: Amount.Def :: Investee.Def :: Investors.Def :: HNil
+
+    override val value: Def = entity("funding_round")
   }
 }
